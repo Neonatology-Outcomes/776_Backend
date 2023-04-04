5e240146cccfb70db9a3b4c9f46a6ae848ffaac8
Ravneet <ravneet@childhealthimprints.com>
2023-04-04 15:25:58 -0500

    Added RulesController and RulesService

--- src/main/java/com/neonatal/backend/controllers/RulesController.java
+++ src/main/java/com/neonatal/backend/controllers/RulesController.java
@@ -1,42 +1,33 @@
 package com.neonatal.backend.controllers;
 
-import com.neonatal.backend.entities.Assessment;
-import com.neonatal.backend.models.ParentBundlePOJO;
-import com.neonatal.backend.repositories.AssessmentRepository;
-import com.neonatal.backend.services.RulesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.neonatal.backend.models.ParentBundlePOJO;
+import com.neonatal.backend.services.RulesService;
 
-/**
- * Class that defines what GET, POST, etc. calls will be connected with which service method
- */
-@RequestMapping("rules")  // Sets the end point for calls related to Rules Design
 @RestController
 public class RulesController {
 
-    @Autowired
-    private RulesService rulesService;
-    @Autowired
-    private AssessmentRepository assessmentRepository;
+	@Autowired
+	RulesService analyticsService;
 
-    @GetMapping
-    public List<Assessment> rules(){ return assessmentRepository.findAll(); } // Get all Rules
-
-    @PostMapping
-    public Assessment addRule(@RequestBody Assessment assessment){ return assessmentRepository.save(assessment); } // Input new rule to the database
-
+	public RulesController() {
+		System.out.println("Analytics Controller Layer");
+	}
 
 	@RequestMapping(value = "/saveBundle/", method = RequestMethod.POST)
 	public ResponseEntity<ParentBundlePOJO> saveBundle(@RequestBody ParentBundlePOJO parentBundlePOJO) {
 
 		System.out.println("hello");
-		ParentBundlePOJO returnObj = rulesService.saveBundle(parentBundlePOJO);
+		ParentBundlePOJO returnObj = analyticsService.saveBundle(parentBundlePOJO);
 		System.out.println("bye 1");
 		return new ResponseEntity<ParentBundlePOJO>(returnObj, HttpStatus.OK);
 	}
-	
+
 }
