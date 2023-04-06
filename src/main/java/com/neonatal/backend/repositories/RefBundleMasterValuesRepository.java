300fac4e4c1b7fda20220ba9cd522ddcf42c71d7
bryancbrandt <88388907+bryancbrandt@users.noreply.github.com>
2023-04-06 16:17:40 -0500

    Update RefBundleMasterValuesRepository.java

--- src/main/java/com/neonatal/backend/repositories/RefBundleMasterValuesRepository.java
+++ src/main/java/com/neonatal/backend/repositories/RefBundleMasterValuesRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * Repository interface for the Assessment table.  This interface functions as the class for CRUD operations
- * on the Ref_Bundle_Master_Values table with ID type of Integer
+ * This interface functions as the class for CRUD operations
+ * on the Ref_Bundle_Master_Values table with ID type of Long.
  */
 @Repository
 public interface RefBundleMasterValuesRepository extends JpaRepository<Ref_Bundle_Master_Values, Long> {
