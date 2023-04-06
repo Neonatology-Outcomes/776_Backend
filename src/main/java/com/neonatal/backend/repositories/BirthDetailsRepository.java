80acf0abd54e7b2ebce5c83144a1177109710d97
bryancbrandt <88388907+bryancbrandt@users.noreply.github.com>
2023-04-06 16:13:37 -0500

    Update BirthDetailsRepository.java

--- src/main/java/com/neonatal/backend/repositories/BirthDetailsRepository.java
+++ src/main/java/com/neonatal/backend/repositories/BirthDetailsRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.math.BigInteger;
 
 /**
- * Repository interface for the Assessment table.  This interface functions as the class for CRUD operations
+ * Repository interface for the birth_details table.  This interface functions as the class for CRUD operations
  * on the Birth_Details table with ID type of BigInteger
  */
 @Repository
