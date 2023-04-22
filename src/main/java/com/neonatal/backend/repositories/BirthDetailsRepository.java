4d0b44a3f34cc1b06e23312815177f2e4ed20e78
KaurRavneetCompSci <kaurR09@uww.edu>
2023-04-22 11:10:45 -0500

    Nurse Tasks done

--- src/main/java/com/neonatal/backend/repositories/BirthDetailsRepository.java
+++ src/main/java/com/neonatal/backend/repositories/BirthDetailsRepository.java
@@ -1,10 +1,12 @@
 package com.neonatal.backend.repositories;
 
 import com.neonatal.backend.entities.Birth_Details;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigInteger;
+import java.util.List;
 
 /**
  * This interface functions as the class for CRUD operations
@@ -12,4 +14,7 @@ import java.math.BigInteger;
  */
 @Repository
 public interface BirthDetailsRepository extends JpaRepository<Birth_Details, Long> {
+	
+	@Query(value = "SELECT * FROM quality.birth_details", nativeQuery = true)
+    public List<Birth_Details> getBirthDetails();
 }
