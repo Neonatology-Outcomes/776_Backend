0fb33ea2d510ea0cb587a07b0d2d42db29d6ecc0
Harlieen Bindra <harlieenbindra@gmail.com>
2023-04-30 18:19:09 -0500

    Committing Registration related files

--- src/main/java/com/neonatal/backend/controllers/RegistrationController.java
+++ src/main/java/com/neonatal/backend/controllers/RegistrationController.java
@@ -1,5 +1,6 @@
 package com.neonatal.backend.controllers;
 
+<<<<<<< Updated upstream
 import com.neonatal.backend.entities.Assessment;
 import com.neonatal.backend.entities.User;
 import com.neonatal.backend.entities.Role;
@@ -102,3 +103,59 @@ public class RegistrationController {
 
    
 }
+=======
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.neonatal.backend.entities.Parent_Bundle;
+import com.neonatal.backend.entities.User;
+import com.neonatal.backend.models.NurseTasks;
+import com.neonatal.backend.models.ParentBundlePOJO;
+import com.neonatal.backend.models.RuleObjectPOJO;
+import com.neonatal.backend.repositories.ParentBundleRepository;
+import com.neonatal.backend.repositories.UserRepository;
+import com.neonatal.backend.services.JwtUtils;
+import com.neonatal.backend.services.RulesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class that defines what GET, POST calls related to the Rules and Actions tables
+ */
+
+@RestController
+public class RegistrationController {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * Gets all bundle information and returns them as a JSON array of rows with values
+     * for: Rule Name, Condition, Action
+     * @return an ArrayList of RUleOBjectPOJO that represents the JSON.
+     */
+    
+   // Show Register page.
+   @RequestMapping(value = "/signup", method = RequestMethod.POST)
+   public String viewRegister(@RequestBody User user) {
+	   String jwtToken;
+	   
+	   jwtToken = jwtUtils.encodeJwt(user.getUsername());
+	   
+	   System.out.println("Adding User to Database Neonatal");
+	   userRepository.save(new User(user.getUsername(), user.getPassword(),
+			   user.getEmailaddress(), user.getFirstname(), user.getLastname(), user.getRoleid()));
+	   long UserId = user.getId();
+       System.out.println("User ID: " + UserId);
+	   return jwtToken;
+   }
+
+    
+
+}
+>>>>>>> Stashed changes
