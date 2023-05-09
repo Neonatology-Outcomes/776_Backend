b058845c4e3ad3009eae300af6c30341f99dac9b
mjthemj <113310787+mjthemj@users.noreply.github.com>
2023-05-09 12:54:02 -0500

    added more test cases

--- src/test/java/com/neonatal/backend/services/RulesServiceTest.java
+++ src/test/java/com/neonatal/backend/services/RulesServiceTest.java
@@ -1,16 +1,20 @@
 package com.neonatal.backend.services;
 
-import com.neonatal.backend.entities.Birth_Details;
-import com.neonatal.backend.entities.Recommendation_Object;
+import com.neonatal.backend.entities.*;
 import com.neonatal.backend.models.NurseTasks;
+import com.neonatal.backend.models.ParentBundlePOJO;
+import com.neonatal.backend.models.RuleObjectPOJO;
+import com.neonatal.backend.models.SubBundlePOJO;
 import com.neonatal.backend.repositories.*;
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import  java.util.Date;
-import java.util.List;
+
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -35,6 +39,91 @@ public class RulesServiceTest {
     @BeforeEach
     public void setup() {
         MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getAllHappyPath(){
+        //setup
+        Sub_Bundle mocksubBundle = new Sub_Bundle();
+        List<Sub_Bundle> mocksubBundleList = List.of(mocksubBundle);
+        Criteria_Bundles mockcriteriaBundles = new Criteria_Bundles();
+        List<Criteria_Bundles> mockCritBundleList = List.of(mockcriteriaBundles);
+        Recommendation_Bundle mockRecBundle = new Recommendation_Bundle();
+        List<Recommendation_Bundle> mockRecBundleList = List.of(mockRecBundle);
+        Criteria_Object mockCritObject = new Criteria_Object();
+        List<Criteria_Object> mockCritObjList = List.of(mockCritObject);
+        Recommendation_Object mockRecObj = new Recommendation_Object();
+        List<Recommendation_Object> mockRecObjList = List.of(mockRecObj);
+
+        when(mocksubBundleRepository.findAll()).thenReturn(mocksubBundleList);
+        when(mockcriteriaBundlesRepository.getBySub_bundle_id(10l)).thenReturn(mockCritBundleList);
+        when(mockrecommendationBundleRepository.getBySub_bundle_id(10l)).thenReturn(mockRecBundleList);
+        when(mockcriteriaObjectRepository.getByCriteria_bundles_id(10l)).thenReturn(mockCritObjList);
+        when(mockrecommendationObjectRepository.getByRecommendation_object_id(10l)).thenReturn(mockRecObjList);
+        //test
+        ArrayList<RuleObjectPOJO> results = rulesServiceUAT.getAll();
+        //assert
+        RuleObjectPOJO mockRuleObjectPOJO = new RuleObjectPOJO(
+                "name",
+                "condition",
+                "action");
+        ArrayList<RuleObjectPOJO> expected = new ArrayList<>();
+        expected.add(mockRuleObjectPOJO);
+        assertEquals(expected, results);
+    }
+
+    @Test
+    public void addRulesHappyPath(){
+        //setup
+        Parent_Bundle mockParentBundle = new Parent_Bundle();
+        Sub_Bundle mockSubBundle = new Sub_Bundle();
+        Criteria_Bundles mockCritBundle = new Criteria_Bundles();
+        Recommendation_Bundle mockRecBundle = new Recommendation_Bundle();
+        Criteria_Object mockCritObj = new Criteria_Object();
+        List<Criteria_Object> mockCritObjList = List.of(mockCritObj);
+        Recommendation_Object mockRecObj = new Recommendation_Object();
+        List<Recommendation_Object> mockRecObjList = List.of(mockRecObj);
+        when(mockparentBundleRepository.save(mockParentBundle)).thenReturn(mockParentBundle);
+        when(mocksubBundleRepository.save(mockSubBundle)).thenReturn(mockSubBundle);
+        when(mockcriteriaBundlesRepository.save(mockCritBundle)).thenReturn(mockCritBundle);
+        when(mockrecommendationBundleRepository.save(mockRecBundle)).thenReturn(mockRecBundle);
+        when(mockcriteriaObjectRepository.saveAll(mockCritObjList)).thenReturn(mockCritObjList);
+        when(mockrecommendationObjectRepository.saveAll(mockRecObjList)).thenReturn(mockRecObjList);
+        List<SubBundlePOJO> mockSubBundleList = List.of(new SubBundlePOJO());
+        ParentBundlePOJO mockParentBundlePOJO = new ParentBundlePOJO(
+                "",
+                "",
+                "",
+                "",
+                mockSubBundleList
+        );
+        //test
+        String results = rulesServiceUAT.addRules(mockParentBundlePOJO);
+        //assert
+        String expected = "success";
+        assertEquals(expected, results);
+    }
+
+    @Test
+    public void mapCriteriaObjectToEntityHappyPath(){
+        //setup
+        Criteria_Object mockCritObj = new Criteria_Object();
+        List<Criteria_Object> mockCritObjList = List.of(mockCritObj);
+        //test
+        //rulesServiceUAT.mapCriteriaObjectToEntity(mockCritObjList, 10l);
+        /*
+        this method is private within RulesService and can't be called
+         */
+        //assert
+    }
+    @Test
+    public void addRecomObjectToEntityHappyPath(){
+        /*
+        like mapCriteriaObjectToEntity, this method is private and can't be called using RulesServiceUAT
+         */
+        //setup
+        //test
+        //assert
     }
     @Test
     public void getNurseTasksHappyPath(){
